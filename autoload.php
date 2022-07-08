ddd07661f83eeb084109cd5441597efad6d49134
Vineet Verma <vineetvrm05@gmail.com>
2022-07-08 21:19:17 +0530

    👌 IMPROVE: folder stucture and autoloader

--- autoload.php
+++ autoload.php
@@ -1,30 +1,38 @@
 <?php
+/**
+ * Special Class Autoloader
+ *
+ * @package WordPress
+ * @subpackage WPBloqs Elementor Addons
+ * @since 1.1
+ */
 
-spl_autoload_register(function ($class) {
+spl_autoload_register(
+	function ( $class ) {
 
-    $prefix = 'WPBLOQS_ELEMENTOR_ADDONS\\';
+		$prefix = 'WPBLOQS_ELEMENTOR_ADDONS\\';
 
-    // base directory for the namespace prefix
-    $base_dir = WPBEL_PLUGIN_PATH . '/inc/';
+		// base directory for the namespace prefix.
+		$base_dir = WPBEL_PLUGIN_PATH . '/inc/';
 
-    // does the class use the namespace prefix?
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        // no, move to the next registered autoloader
-        return;
-    }
+		// does the class use the namespace prefix?
+		$len = strlen( $prefix );
+		if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+			// no, move to the next registered autoloader.
+			return;
+		}
 
-    // get the relative class name
-    $relative_class = strtolower(substr($class, $len));
+		// get the relative class name.
+		$relative_class = strtolower( substr( $class, $len ) );
 
+		// replace the namespace prefix with the base directory, replace namespace
+		// separators with directory separators in the relative class name, append
+		// with .php.
+		$file = $base_dir . str_replace( array( '\\', '_' ), array( '/', '-' ), $relative_class ) . '.php';
 
-    // replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the relative class name, append
-    // with .php
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    // if the file exists, require it
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+		// if the file exists, require it.
+		if ( file_exists( $file ) ) {
+			require $file;
+		}
+	}
+);
---
+++ inc/dynamic-tags/dynamic-tags-loader.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Dynamic Tags Loader Class
+ *
+ * @package WordPress
+ * @subpackage WPBloqs Elementor Addon
+ * @since 1.0.0
+ */
+
+namespace WPBLOQS_ELEMENTOR_ADDONS\DYNAMIC_TAGS;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Dynamic_Tags_Loader {
+
+	public function __construct() {
+		add_action( 'elementor/dynamic_tags/register', array( $this, 'register_site_dynamic_tag_group' ) );
+		add_action( 'elementor/dynamic_tags/register', array( $this, 'register_tags' ) );
+
+		// enabling HTML Output for renders
+		add_action( 'elementor/frontend/the_content', array( $this, 'render' ) );
+		add_action( 'elementor/widget/render_content', array( $this, 'render' ) );
+	}
+
+	public function register_tags( $dynamic_tags_manager ) {
+		// Reading Time Tag
+		$dynamic_tags_manager->register( new \WPBLOQS_ELEMENTOR_ADDONS\DYNAMIC_TAGS\READING_TIME\Reading_Time_Tag() );
+		// Last Updated Tag
+		$dynamic_tags_manager->register( new \WPBLOQS_ELEMENTOR_ADDONS\DYNAMIC_TAGS\LAST_UPDATED\Last_Updated_Tag() );
+	}
+
+	public function register_site_dynamic_tag_group( $dynamic_tags_manager ) {
+
+		$dynamic_tags_manager->register_group(
+			'wpbloqs',
+			array(
+				'title' => esc_html__( 'WPBloqs', 'wpbloqs-elementor-addon' ),
+			)
+		);
+	}
+
+	public function render( $content ) {
+
+		$updated_start_tag   = '<time itemprop="dateModified" datetime="' . get_post_modified_time( 'Y-m-d\TH:i:sP', true ) . '">';
+		$published_start_tag = '<time itemprop="datePublished" datetime="' . get_the_date( 'Y-m-d\TH:i:sP' ) . '">';
+		$end_tag             = '</time>';
+
+		$content = str_replace( '%updated_schema_start%', $updated_start_tag, $content );
+		$content = str_replace( '%published_schema_start%', $published_start_tag, $content );
+		$content = str_replace( '%date_schema_end%', $end_tag, $content );
+
+		return $content;
+	}
+}
