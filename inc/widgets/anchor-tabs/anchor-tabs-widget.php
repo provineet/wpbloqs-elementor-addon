b1703b7b17ec8b3a9f1adb31290175ac5e02e090
Vineet Verma <vineetvrm05@gmail.com>
2022-07-13 01:14:59 +0530

    👌 IMPROVE: Anchor Widget

--- inc/widgets/anchor-tabs/anchor-tabs-widget.php
+++ inc/widgets/anchor-tabs/anchor-tabs-widget.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace WPBLOQS_ELEMENTOR_ADDONS\WIDGETS\ANCHOR_TABS;
+	namespace WPBLOQS_ELEMENTOR_ADDONS\WIDGETS\ANCHOR_TABS;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -14,8 +14,9 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Retrieve list widget name.
 	 *
-	 * @since 1.0.0
 	 * @access public
+	 * @since 1.0.0
+	 *
 	 * @return string Widget name.
 	 */
 	public function get_name() {
@@ -27,12 +28,13 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Retrieve list widget title.
 	 *
-	 * @since 1.0.0
 	 * @access public
+	 * @since 1.0.0
+	 *
 	 * @return string Widget title.
 	 */
 	public function get_title() {
-		return esc_html__( 'Anchor Tabs', 'elementor-list-widget' );
+		return esc_html__( 'Anchor Tabs', 'wpbloqs-elementor-addon' );
 	}
 
 	/**
@@ -40,8 +42,9 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Retrieve list widget icon.
 	 *
-	 * @since 1.0.0
 	 * @access public
+	 * @since 1.0.0
+	 *
 	 * @return string Widget icon.
 	 */
 	public function get_icon() {
@@ -53,8 +56,9 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Retrieve a URL where the user can get more information about the widget.
 	 *
-	 * @since 1.0.0
 	 * @access public
+	 * @since 1.0.0
+	 *
 	 * @return string Widget help URL.
 	 */
 	public function get_custom_help_url() {
@@ -66,8 +70,9 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Retrieve the list of categories the list widget belongs to.
 	 *
-	 * @since 1.0.0
 	 * @access public
+	 * @since 1.0.0
+	 *
 	 * @return array Widget categories.
 	 */
 	public function get_categories() {
@@ -79,8 +84,9 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Retrieve the list of keywords the list widget belongs to.
 	 *
-	 * @since 1.0.0
 	 * @access public
+	 * @since 1.0.0
+	 *
 	 * @return array Widget keywords.
 	 */
 	public function get_keywords() {
@@ -92,17 +98,20 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Return the JS files handles required by the widget
 	 *
-	 * @since 1.0.0
 	 * @access public
+	 * @since 1.0.0
+	 *
 	 * @return array JS Handles.
 	 */
 	public function get_script_depends() {
 
-		// wp_register_script('widget-script-1', plugins_url('assets/js/widget-script-1.js', __FILE__));
+		$script_version = filemtime( WPBEL_ASSET_URL . '/js/wpb_scripts.js' );
 
-		// return [
-		// 'widget-script-1'
-		// ];
+		wp_register_script( 'wpb_scripts', WPBEL_ASSET_URL . '/js/wpb_scripts.js', array(), $script_version, true );
+
+		return array(
+			'wpb_scripts',
+		);
 	}
 
 	/**
@@ -110,13 +119,14 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Return the CSS files handles required by the widget
 	 *
-	 * @since 1.0.0
 	 * @access public
+	 * @since 1.0.0
+	 *
 	 * @return array Widget keywords.
 	 */
 	public function get_style_depends() {
 
-		$style_version = filemtime( WPBEL_ASSET_URL . 'css/wpb_widgets.css' );
+		$style_version = filemtime( WPBEL_ASSET_URL . '/css/wpb_widgets.css' );
 
 		wp_register_style( 'wpbloqs-styles', WPBEL_ASSET_URL . '/css/wpb_widgets.css', array(), $style_version, 'all' );
 
@@ -130,143 +140,138 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Add input fields to allow the user to customize the widget settings.
 	 *
-	 * @since 1.0.0
 	 * @access protected
+	 * @since 1.0.0
 	 */
 	protected function register_controls() {
 
 		$this->start_controls_section(
 			'content_section',
 			array(
-				'label' => esc_html__( 'Tabs Content', 'elementor-list-widget' ),
+				'label' => esc_html__( 'Content', 'plugin-name' ),
 				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
 			)
 		);
 
-		$this->add_control(
-			'list_style',
-			array(
-				'label'   => esc_html__( 'Layout', 'plugin-name' ),
-				'type'    => \Elementor\Controls_Manager::CHOOSE,
-				'options' => array(
-					'default' => array(
-						'title' => esc_html__( 'Default', 'plugin-name' ),
-						'icon'  => 'eicon-editor-list-ul',
-					),
-					'inline'  => array(
-						'title' => esc_html__( 'Inline', 'plugin-name' ),
-						'icon'  => 'eicon-ellipsis-h',
-					),
-				),
-				'default' => 'inline',
-				'toggle'  => true,
-			)
-		);
+				$this->add_control(
+					'list_style',
+					array(
+						'label'   => esc_html__( 'Layout', 'wpbloqs-elementor-addon' ),
+						'type'    => \Elementor\Controls_Manager::CHOOSE,
+						'options' => array(
+							'default' => array(
+								'title' => esc_html__( 'Default', 'wpbloqs-elementor-addon' ),
+								'icon'  => 'eicon-editor-list-ul',
+							),
+							'inline'  => array(
+								'title' => esc_html__( 'Inline', 'wpbloqs-elementor-addon' ),
+								'icon'  => 'eicon-ellipsis-h',
+							),
+						),
+						'default' => 'inline',
+						'toggle'  => true,
+					)
+				);
 
-		$this->add_control(
-			'tab_type',
-			array(
-				'label'        => esc_html__( 'Tab Type', 'plugin-name' ),
-				'type'         => \Elementor\Controls_Manager::SWITCHER,
-				'label_on'     => esc_html__( 'Meta', 'your-plugin' ),
-				'label_off'    => esc_html__( 'Custom', 'your-plugin' ),
-				'return_value' => 'yes',
-				'default'      => 'yes',
-			)
-		);
-
-		/* Start repeater */
+			$this->add_control(
+				'tab_type',
+				array(
+					'label'        => esc_html__( 'Tab Type', 'wpbloqs-elementor-addon' ),
+					'type'         => \Elementor\Controls_Manager::SWITCHER,
+					'label_on'     => esc_html__( 'Meta', 'wpbloqs-elementor-addon' ),
+					'label_off'    => esc_html__( 'Custom', 'wpbloqs-elementor-addon' ),
+					'return_value' => 'yes',
+					'default'      => 'yes',
+				)
+			);
 
 		$repeater = new \Elementor\Repeater();
 
 		$repeater->add_control(
 			'label',
 			array(
-				'label'       => esc_html__( 'Label', 'elementor-list-widget' ),
+				'label'       => esc_html__( 'Label', 'wpbloqs-elementor-addon' ),
 				'type'        => \Elementor\Controls_Manager::TEXT,
-				'placeholder' => esc_html__( 'Tab Label', 'elementor-list-widget' ),
-				'default'     => esc_html__( 'Tab Label', 'elementor-list-widget' ),
+				'placeholder' => esc_html__( 'Tab Label', 'wpbloqs-elementor-addon' ),
+				'default'     => esc_html__( 'Tab Label', 'wpbloqs-elementor-addon' ),
 				'label_block' => true,
 			)
 		);
 
-		$repeater->add_control(
-			'link',
-			array(
-				'label'       => esc_html__( 'Anchor', 'elementor-list-widget' ),
-				'type'        => \Elementor\Controls_Manager::TEXT,
-				'placeholder' => esc_html__( 'Anchor Name', 'elementor-list-widget' ),
-				'label_block' => true,
-			)
-		);
+			$repeater->add_control(
+				'link',
+				array(
+					'label'       => esc_html__( 'Anchor', 'wpbloqs-elementor-addon' ),
+					'type'        => \Elementor\Controls_Manager::TEXT,
+					'placeholder' => esc_html__( 'Anchor Name', 'wpbloqs-elementor-addon' ),
+					'label_block' => true,
+				)
+			);
 
-		$repeater->add_control(
-			'icon-set',
-			array(
-				'label'   => esc_html__( 'Icon', 'plugin-name' ),
-				'type'    => \Elementor\Controls_Manager::SELECT,
-				'default' => 'none',
-				'options' => array(
-					'none'   => esc_html__( 'None', 'plugin-name' ),
-					'custom' => esc_html__( 'Custom', 'plugin-name' ),
-				),
-			)
-		);
-
-		$repeater->add_control(
-			'icon',
-			array(
-				'label'       => esc_html__( '', 'plugin-name' ),
-				'type'        => \Elementor\Controls_Manager::ICONS,
-				'default'     => array(
-					'value'   => 'fas fa-circle',
-					'library' => 'fa-solid',
-				),
-				'recommended' => array(
-					'fa-solid'   => array(
-						'circle',
-						'dot-circle',
-						'square-full',
+			$repeater->add_control(
+				'icon-set',
+				array(
+					'label'   => esc_html__( 'Icon', 'wpbloqs-elementor-addon' ),
+					'type'    => \Elementor\Controls_Manager::SELECT,
+					'default' => 'none',
+					'options' => array(
+						'none'   => esc_html__( 'None', 'wpbloqs-elementor-addon' ),
+						'custom' => esc_html__( 'Custom', 'wpbloqs-elementor-addon' ),
 					),
-					'fa-regular' => array(
-						'circle',
-						'dot-circle',
-						'square-full',
-					),
-				),
-				'condition'   => array(
-					'icon-set' => 'custom',
-				),
-			)
-		);
+				)
+			);
 
-		/* End repeater */
-
-		$this->add_control(
-			'tab_items',
-			array(
-				'label'       => esc_html__( 'Tab Items', 'elementor-list-widget' ),
-				'type'        => \Elementor\Controls_Manager::REPEATER,
-				'fields'      => $repeater->get_controls(),           /* Use our repeater */
-				'default'     => array(
-					array(
-						'text' => esc_html__( 'Tab #1', 'elementor-list-widget' ),
-						'link' => '#anchor',
+			$repeater->add_control(
+				'icon',
+				array(
+					'type'        => \Elementor\Controls_Manager::ICONS,
+					'default'     => array(
+						'value'   => 'fas fa-circle',
+						'library' => 'fa-solid',
 					),
-				),
-				'title_field' => '{{{ text }}}',
-				'condition'   => array(
-					'tab_type' => '',
-				),
-			)
-		);
+					'recommended' => array(
+						'fa-solid'   => array(
+							'circle',
+							'dot-circle',
+							'square-full',
+						),
+						'fa-regular' => array(
+							'circle',
+							'dot-circle',
+							'square-full',
+						),
+					),
+					'condition'   => array(
+						'icon-set' => 'custom',
+					),
+				)
+			);
+
+			$this->add_control(
+				'tab_items',
+				array(
+					'label'       => esc_html__( 'Tab Items', 'wpbloqs-elementor-addon' ),
+					'type'        => \Elementor\Controls_Manager::REPEATER,
+					'fields'      => $repeater->get_controls(), /* Use our repeater */
+					'default'     => array(
+						array(
+							'label' => esc_html__( 'Tab #1', 'wpbloqs-elementor-addon' ),
+							'link'  => '#anchor',
+						),
+					),
+					'title_field' => '{{{ label }}}',
+					'condition'   => array(
+						'tab_type' => '',
+					),
+				)
+			);
 
 		$this->end_controls_section();
 
 		$this->start_controls_section(
 			'style_content_section',
 			array(
-				'label' => esc_html__( 'Tab Style', 'elementor-list-widget' ),
+				'label' => esc_html__( 'Tab Style', 'wpbloqs-elementor-addon' ),
 				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
 			)
 		);
@@ -274,35 +279,38 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 		$this->add_control(
 			'text_align',
 			array(
-				'label'   => esc_html__( 'Alignment', 'plugin-name' ),
-				'type'    => \Elementor\Controls_Manager::CHOOSE,
-				'options' => array(
+				'label'     => esc_html__( 'Alignment', 'wpbloqs-elementor-addon' ),
+				'type'      => \Elementor\Controls_Manager::CHOOSE,
+				'options'   => array(
 					'left'   => array(
-						'title' => esc_html__( 'Left', 'plugin-name' ),
+						'title' => esc_html__( 'Left', 'wpbloqs-elementor-addon' ),
 						'icon'  => 'eicon-text-align-left',
 					),
 					'center' => array(
-						'title' => esc_html__( 'Center', 'plugin-name' ),
+						'title' => esc_html__( 'Center', 'wpbloqs-elementor-addon' ),
 						'icon'  => 'eicon-text-align-center',
 					),
 					'right'  => array(
-						'title' => esc_html__( 'Right', 'plugin-name' ),
+						'title' => esc_html__( 'Right', 'wpbloqs-elementor-addon' ),
 						'icon'  => 'eicon-text-align-right',
 					),
 				),
-				'default' => 'center',
-				'toggle'  => true,
+				'default'   => 'center',
+				'toggle'    => true,
+				'selectors' => array(
+					'{{WRAPPER}} ul' => 'text-align: {{VALUE}}',
+				),
 			)
 		);
 
 		$this->add_control(
 			'title_color',
 			array(
-				'label'     => esc_html__( 'Color', 'elementor-list-widget' ),
+				'label'     => esc_html__( 'Color', 'wpbloqs-elementor-addon' ),
 				'type'      => \Elementor\Controls_Manager::COLOR,
 				'selectors' => array(
-					'{{WRAPPER}} .elementor-list-widget-text' => 'color: {{VALUE}};',
-					'{{WRAPPER}} .elementor-list-widget-text > a' => 'color: {{VALUE}};',
+					'{{WRAPPER}} ul li' => 'color: {{VALUE}};',
+					'{{WRAPPER}} a'     => 'color: {{VALUE}};',
 				),
 			)
 		);
@@ -311,7 +319,7 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 			\Elementor\Group_Control_Typography::get_type(),
 			array(
 				'name'     => 'icon_typography',
-				'selector' => '{{WRAPPER}} .elementor-list-widget-text, {{WRAPPER}} .elementor-list-widget-text > a',
+				'selector' => '{{WRAPPER}} li a',
 			)
 		);
 
@@ -319,7 +327,7 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 			\Elementor\Group_Control_Text_Shadow::get_type(),
 			array(
 				'name'     => 'text_shadow',
-				'selector' => '{{WRAPPER}} .elementor-list-widget-text',
+				'selector' => '{{WRAPPER}} ul li a',
 			)
 		);
 
@@ -327,20 +335,20 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 			\Elementor\Group_Control_Background::get_type(),
 			array(
 				'name'     => 'background',
-				'label'    => esc_html__( 'Background', 'plugin-name' ),
+				'label'    => esc_html__( 'Background', 'wpbloqs-elementor-addon' ),
 				'types'    => array( 'classic', 'gradient', 'video' ),
-				'selector' => '{{WRAPPER}} .your-class',
+				'selector' => '{{WRAPPER}} ul li a',
 			)
 		);
 
 		$this->add_control(
 			'padding',
 			array(
-				'label'      => esc_html__( 'Padding', 'plugin-name' ),
+				'label'      => esc_html__( 'Padding', 'wpbloqs-elementor-addon' ),
 				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
 				'size_units' => array( 'px', '%', 'em' ),
 				'selectors'  => array(
-					'{{WRAPPER}} .your-class' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} ul li a' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				),
 			)
 		);
@@ -348,11 +356,23 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 		$this->add_control(
 			'margin',
 			array(
-				'label'      => esc_html__( 'Margin', 'plugin-name' ),
+				'label'      => esc_html__( 'Margin', 'wpbloqs-elementor-addon' ),
 				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
 				'size_units' => array( 'px', '%', 'em' ),
 				'selectors'  => array(
-					'{{WRAPPER}} .your-class' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} ul li' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				),
+			)
+		);
+
+		$this->add_control(
+			'border',
+			array(
+				'label'      => esc_html__( 'Border Radius', 'wpbloqs-elementor-addon' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => array( 'px', '%', 'em' ),
+				'selectors'  => array(
+					'{{WRAPPER}} ul li a' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				),
 			)
 		);
@@ -362,7 +382,7 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 		$this->start_controls_section(
 			'style_icon_section',
 			array(
-				'label' => esc_html__( 'Icon Style', 'elementor-list-widget' ),
+				'label' => esc_html__( 'Icon Style', 'wpbloqs-elementor-addon' ),
 				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
 			)
 		);
@@ -370,7 +390,7 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 		$this->add_control(
 			'icon_color',
 			array(
-				'label'     => esc_html__( 'Color', 'elementor-list-widget' ),
+				'label'     => esc_html__( 'Color', 'wpbloqs-elementor-addon' ),
 				'type'      => \Elementor\Controls_Manager::COLOR,
 				'selectors' => array(
 					'{{WRAPPER}} .elementor-list-widget-text::marker' => 'color: {{VALUE}};',
@@ -381,7 +401,7 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 		$this->add_control(
 			'icon_size',
 			array(
-				'label'      => esc_html__( 'Size', 'elementor-list-widget' ),
+				'label'      => esc_html__( 'Size', 'wpbloqs-elementor-addon' ),
 				'type'       => \Elementor\Controls_Manager::SLIDER,
 				'size_units' => array( 'px', 'em', 'rem' ),
 				'range'      => array(
@@ -408,7 +428,7 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 		$this->add_control(
 			'icon_spacing',
 			array(
-				'label'      => esc_html__( 'Spacing', 'elementor-list-widget' ),
+				'label'      => esc_html__( 'Spacing', 'wpbloqs-elementor-addon' ),
 				'type'       => \Elementor\Controls_Manager::SLIDER,
 				'size_units' => array( 'px', 'em', 'rem' ),
 				'range'      => array(
@@ -444,13 +464,13 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 	 *
 	 * Written in PHP and used to generate the final HTML.
 	 *
-	 * @since 1.0.0
 	 * @access protected
+	 * @since 1.0.0
 	 */
 	protected function render() {
 		$settings = $this->get_settings_for_display();
 
-		if ( $settings['list_style'] === 'inline' ) {
+		if ( 'inline' === $settings['list_style'] ) {
 			$this->add_render_attribute( 'list', 'class', 'wpbloqs-anchor-widget wpbloqs-list wpbloqs-inline-list' );
 		} else {
 			$this->add_render_attribute( 'list', 'class', 'wpbloqs-anchor-widget wpbloqs-list' );
@@ -460,13 +480,13 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 <ul <?php $this->print_render_attribute_string( 'list' ); ?>>
 	<?php
 		foreach ( $settings['tab_items'] as $index => $item ) {
-			$repeater_setting_key = $this->get_repeater_setting_key( 'label', 'tab_items', $index );
-			$this->add_render_attribute( $repeater_setting_key, 'class', 'elementor-list-widget-text' );
-			$this->add_inline_editing_attributes( $repeater_setting_key );
+					$repeater_setting_key = $this->get_repeater_setting_key( 'label', 'tab_items', $index );
+					$this->add_render_attribute( $repeater_setting_key, 'class', 'elementor-list-widget-text' );
+					$this->add_inline_editing_attributes( $repeater_setting_key );
 			?>
 	<li <?php $this->print_render_attribute_string( $repeater_setting_key ); ?>>
 		<?php
-				$title = $item['label'];
+			$title = $item['label'];
 
 			if ( ! empty( $item['link'] ) ) {
 				$item['link'] = array(
@@ -476,8 +496,6 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 				$this->add_link_attributes( "link_{$index}", $item['link'] );
 				$linked_title = sprintf( '<a %1$s>%2$s</a>', $this->get_render_attribute_string( "link_{$index}" ), $title );
 				echo $linked_title;
-			} else {
-				echo $title;
 			}
 			?>
 	</li>
@@ -486,34 +504,5 @@ class Anchor_Tabs_Widget extends \Elementor\Widget_Base {
 		?>
 </ul>
 <?php
-	}
-
-	/**
-	 * Render list widget output in the editor.
-	 *
-	 * Written as a Backbone JavaScript template and used to generate the live preview.
-	 *
-	 * @since 1.0.0
-	 * @access protected
-	 */
-	protected function content_template() {
-		?>
-<# html_tag={ 'ordered' : 'ol' , 'unordered' : 'ul' , 'other' : 'ul' , }; view.addRenderAttribute( 'list' , 'class' , 'elementor-list-widget' ); #>
-	<{{{ html_tag[ settings.marker_type ] }}} {{{ view.getRenderAttributeString( 'list' ) }}}>
-		<# _.each( settings.list_items, function( item, index ) { var repeater_setting_key=view.getRepeaterSettingKey( 'text' , 'list_items' , index ); view.addRenderAttribute( repeater_setting_key, 'class' , 'elementor-list-widget-text' ); view.addInlineEditingAttributes( repeater_setting_key ); #>
-			<li {{{ view.getRenderAttributeString( repeater_setting_key ) }}}>
-				<# var title=item.text; #>
-					<# if ( item.link ) { #>
-						<# view.addRenderAttribute( `link_${index}`, item.link ); #>
-							<a href="{{ item.link.url }}" {{{ view.getRenderAttributeString( `link_${index}` ) }}}>
-								{{{title}}}
-							</a>
-							<# } else { #>
-								{{{title}}}
-								<# } #>
-			</li>
-			<# } ); #>
-	</{{{ html_tag[ settings.marker_type ] }}}>
-	<?php
 	}
 }
